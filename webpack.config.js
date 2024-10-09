1543506d2c576fe1330efde2bbec6de77f7b823c
stillcorners <yheedev@gmail.com>
2024-10-09 14:05:53 +0900

    수정..

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ module.exports = {
   entry: './src/index.tsx',
   output: {
     filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, '../dist'),
+    // path: path.resolve(__dirname, '../dist'),
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
   resolve: {
