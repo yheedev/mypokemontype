1b55daebbbef33b3a305ded0f1c091442ba94e13
stillcorners <yheedev@gmail.com>
2024-10-21 14:43:03 +0900

    언어 바꿔도 이전 언어로 path가 나오던걸 수정

--- src/features/languageSlice.tsx
+++ src/features/languageSlice.tsx
@@ -3,6 +3,21 @@ import ko from '../json/ko.json';
 import en from '../json/en.json';
 import ja from '../json/ja.json';
 
+const getInitialLang = (): 'ko' | 'en' | 'ja' => {
+  const localStorageLang = localStorage.getItem('lang');
+  if (localStorageLang) {
+    return localStorageLang as 'ko' | 'en' | 'ja';
+  }
+
+  const pathLang = window.location.pathname.split('/')[1];
+  if (pathLang === 'en' || pathLang === 'ja' || pathLang === 'ko') {
+    console.log(`Path Language: ${pathLang}`);
+    return pathLang as 'ko' | 'en' | 'ja';
+  }
+
+  return 'ko';
+};
+
 const langs = {
   ko: ko,
   en: en,
@@ -14,7 +29,7 @@ export type langState = {
   translations: typeof ko;
 };
 
-const initialLang = 'ko';
+const initialLang = getInitialLang();
 
 export const languageSlice = createSlice({
   name: 'language',
@@ -33,35 +48,3 @@ export const languageSlice = createSlice({
 
 export const { language } = languageSlice.actions;
 export default languageSlice.reducer;
-
-// 기존의 대문자 국가 코드들을 소문자로 변경하는 작업은 cloudfront function에서 처리 완료 후 진행
-// const EngCode = ['us', 'ca', 'gb', 'au', 'nz', 'ie', 'za'];
-
-// const getInitialLang = (): 'ko' | 'en' | 'ja' => {
-//   const localStorageLang = localStorage.getItem('lang');
-//   if (localStorageLang) {
-//     console.log(`LocalStorage Language: ${localStorageLang}`);
-//     return localStorageLang as 'ko' | 'en' | 'ja';
-//   }
-
-//   // URL 경로에서 언어 코드 추출
-//   // const pathLang = window.location.pathname.split('/')[1];
-//   // if (pathLang === 'en' || pathLang === 'ja' || pathLang === 'ko') {
-//   //   console.log(`Path Language: ${pathLang}`);
-//   //   return pathLang as 'ko' | 'en' | 'ja';
-//   // }
-
-//   const viewerCountry = (window as any).viewerCountry;
-//   if (viewerCountry) {
-//     const countryCode = viewerCountry.toLowerCase();
-//     console.log(`Viewer Country: ${countryCode}`);
-//     if (EngCode.includes(countryCode)) {
-//       return 'en';
-//     } else if (countryCode === 'jp') {
-//       return 'ja';
-//     } else {
-//       return 'ko';
-//     }
-//   }
-//   return 'ko';
-// };
--- src/pages/RootRoute.tsx
+++ src/pages/RootRoute.tsx
@@ -6,7 +6,7 @@ import More from './More';
 function RootRoute() {
   return (
     <Routes>
-      <Route path="/" element={<Offense />} />
+      {/* <Route path="/" element={<Offense />} /> */}
       <Route path="/:lang" element={<Offense />} />
       <Route path="/:lang/defense" element={<Defense />} />
       <Route path="/:lang/more" element={<More />} />
