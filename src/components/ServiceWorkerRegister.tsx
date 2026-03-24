ba7dccf9b9e122da5a1df682fbc39d879136f907
yheedev <yheedev@gmail.com>
2026-03-24 16:28:57 +0900

    feat: 서비스워커 등록 조건을 프로덕션으로 제한

--- src/components/ServiceWorkerRegister.tsx
+++ src/components/ServiceWorkerRegister.tsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react'
 
 export function ServiceWorkerRegister() {
   useEffect(() => {
-    if ('serviceWorker' in navigator) {
+    if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
       navigator.serviceWorker.register('/sw.js').catch(() => {})
     }
   }, [])
